7bd79ce6f0f9b32deacdd8116a676ee73088ff2b
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-03-01 11:54:32 +0100

    initialized all of the route handlers

--- index.js
+++ index.js
@@ -25,56 +25,93 @@ const getAllBloggers = (req, res) => {
   });
 };
 
-const createPost = (req, res) => {};
+const createPost = (req, res) => {
+  res.status(201).json({
+    status: "success",
+    message: "Post created successfully",
+  });
+};
 // get a blogger by id
-const getBloggerById = (req, res) => {};
+const getBloggerById = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Blogger fetched successfully",
+  });
+};
 
 // get all posts of a blogger by id
-const getBloggerPosts = (req, res) => {};
+const getBloggerPosts = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "All posts fetched successfully",
+  });
+};
 
 // get a particularpost of a blogger by post id
 
-const singlePost = (req, res) => {};
+const singlePost = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Post fetched successfully",
+  });
+};
 // delete a post of a blogger by post id
-const deletePost = (req, res) => {};
+const deletePost = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Post deleted successfully",
+  });
+};
 // update a post of a blogger by post id
-const updatePost = (req, res) => {};
+const updatePost = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Post updated successfully",
+  });
+};
 
 // delete a blogger by id
-const deleteBlogger = (req, res) => {};
+const deleteBlogger = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Blogger deleted successfully",
+  });
+};
 // update a blogger by id
-const updateBlogger = (req, res) => {};
+const updateBlogger = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Blogger updated successfully",
+  });
+};
 // get all posts
-const getAllPosts = (req, res) => {};
+const getAllPosts = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "All posts fetched successfully",
+  });
+};
 // routes
-app.route("api/v1/bloggers").get(getAllBloggers).post(createBlogger);
+app.route("/api/v1/bloggers").get(getAllBloggers).post(createBlogger);
 app
-  .route("api/v1/bloggers/:id")
+  .route("/api/v1/bloggers/:id")
   .get(getBloggerById)
   .delete(deleteBlogger)
   .patch(updateBlogger);
 //
 
-app.route("api/v1/bloggers/:id/posts").get(getBloggerPosts).post(createPost);
+app.route("/api/v1/bloggers/:id/posts").get(getBloggerPosts).post(createPost);
 //
 
 app
-  .route("api/v1/bloggers/:id/posts/:postid")
+  .route("/api/v1/bloggers/:id/posts/:postid")
   .get(singlePost)
   .delete(deletePost)
   .patch(updatePost);
 
-app.route("api/v1/posts").get(getAllPosts);
+app.route("/api/v1/posts").get(getAllPosts);
 
 // Start the server
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
-
-//  i want to be able to:
-/* 
-
-
-
-get all posts,
-*/
