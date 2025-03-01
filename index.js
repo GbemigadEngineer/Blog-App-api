22e62b1c47b56e5200d33e06aa66015435510918
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-03-01 11:38:40 +0100

    created more routes and route handlers

--- index.js
+++ index.js
@@ -32,11 +32,35 @@ const getBloggerById = (req, res) => {};
 // get all posts of a blogger by id
 const getBloggerPosts = (req, res) => {};
 
+// get a particularpost of a blogger by post id
+
+const singlePost = (req, res) => {};
+// delete a post of a blogger by post id
+const deletePost = (req, res) => {};
+// update a post of a blogger by post id
+const updatePost = (req, res) => {};
+
+// delete a blogger by id
+const deleteBlogger = (req, res) => {};
+// update a blogger by id
+const updateBlogger = (req, res) => {};
 // routes
 // app.route("api/v1/createpost").post(createPost);
 app.route("api/v1/bloggers").get(getAllBloggers).post(createBlogger);
-app.route("api/v1/bloggers/:id/").get(getBloggerById);
+app
+  .route("api/v1/bloggers/:id")
+  .get(getBloggerById)
+  .delete(deleteBlogger)
+  .patch(updateBlogger);
+app.route("api/v1/bloggers/:id/posts").post(createPost);
 app.route("api/v1/bloggers/:id/posts").get(getBloggerPosts);
+app
+  .route("api/v1/bloggers/:id/posts/:postid")
+  .get(singlePost)
+  .delete(deletePost)
+  .patch(updatePost);
+
+// Start the server
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
@@ -44,11 +68,7 @@ app.listen(4000, () => {
 //  i want to be able to:
 /* 
 
-create a post for a blogger or patch a post to the blogger posts object/ array,
 
-delete a post of a blogger by id,
-update a post of a blogger by id,
-delete a blogger by id,
-update a blogger by id,
+
 get all posts,
 */
