f310b2a62723b5fa5a1dc321ff64003af1dc6d58
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-03-19 10:06:47 +0100

    connected my app to my hosted database with mongoose

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 "use strict";
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./index");
-// Start the server
 
+// DATABASE CONNECTION
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+// Using mongoose to connect to our db
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((connectionobj) => {
+    console.log(connectionobj);
+    console.log("Database connection successfull!");
+  });
+// Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}... and it is running in ${process.env.NODE_ENV} enviroment`);
+  console.log(
+    `App running on port ${port}... and it is running in ${process.env.NODE_ENV} enviroment`
+  );
 });
